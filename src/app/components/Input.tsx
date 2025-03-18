f98d10a1246d17939ab72f4506810b093ea5dc83
rohansinha01 <rohansinha01@gmail.com>
2025-03-18 14:19:39 -0400

    first commit

---
+++ src/app/components/Input.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div className='text-amber-50'>
+        <label htmlFor="my-select">Choose an option:</label>
+<select id="my-select" name="my-select">
+  <option value="option1">Option 1</option>
+  <option value="option2">Option 2</option>
+  <option value="option3">Option 3</option>
+</select>
+    </div>
+  )
+}
+
+export default Input
